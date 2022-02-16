ea5146a90daa1255fda4ca9136a01096a9cd0af4
titus mahwenda <titus.mahwenda@pegiondata.com>
2022-02-16 07:07:38 +0200

    backlog environment variables file

---
+++ backlog_env_variables_install.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+DBNAME='DBNAME="mobalytics"'
+DBUSER='DBUSER="mob_db_user"'
+DBPASS='DBPASS="mob_db_pass"'
+DBHOST='DBHOST="localhost"'
+DBPORT="DBPORT=5432"
+
+echo -e  "$DBNAME  \n$DBUSER  \n$DBPASS  \n$DBHOST  \n$DBPORT" >> ~/.bashrc
+
+
