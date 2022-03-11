96bc183d653cd38a57dce1adf64a9e6ef2ec17b4
titus-mahwenda <99360245+titus-mahwenda@users.noreply.github.com>
2022-03-11 06:37:33 +0200

    Update react_web_app.sh

--- sprint2/react_web_app.sh
+++ sprint2/react_web_app.sh
@@ -1,11 +1,17 @@
-#!/usr/bin/
+#!/usr/bin/bash
 
-#Step One
+#Navigating to web app files
+chown -R mob_app_user /var/www/html
+chmod -R 755 /var/www/html
+sudo -u mob_app_user bash -c "cd /var/www/html"
 
+#Create react project
 sudo npx create-react-app react-deploy
 
-sudo -u mob_app_user sh -c "cd react-deploy"
-
+#Build project files
+sudo -u mob_app_user bash -c "cd react-deploy"
 sudo npm run build
 
+#Run the project
 sudo npm start
+
